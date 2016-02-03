9745601fa7a0525807fa2a2b87a8ffc02045be19
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-03 02:36:51 -0200

    Adds UserMenu.js

---
+++ app/components/UserMenu.js
@@ -0,0 +1,17 @@
+import React, {
+  Component,
+  View,
+  Text,
+} from 'react-native'
+
+import Colors from "../styles/Colors"
+import StyleSheets from "../styles/StyleSheets"
+
+export default class UserMenu extends Component {
+  render() {
+    return (
+      <View style={StyleSheets.menu}>
+      </View>
+    )
+  }
+}
