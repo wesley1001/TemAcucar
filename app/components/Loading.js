46156c7644254f25c64c781a726b774ca293246f
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-07 14:45:05 -0200

    Refactors Loading as a functional component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,17 +1,9 @@
-import React, {
-  Component,
-} from 'react-native'
+import React from 'react-native'
 import GiftedSpinner from 'react-native-gifted-spinner'
-
-import StyleSheets from "../styles/StyleSheets"
 import SimpleScreen from "./SimpleScreen"
 
-export default class Loading extends Component {
-  render() {
-    return (
-      <SimpleScreen>
-        <GiftedSpinner style={{ marginVertical: 36 }} />
-      </SimpleScreen>
-    )
-  }
-}
+export default Loading = () => (
+  <SimpleScreen>
+    <GiftedSpinner style={{ marginVertical: 36 }} />
+  </SimpleScreen>
+)
