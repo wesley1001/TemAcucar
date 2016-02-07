617230945d1c732b4878af462675c282ffddbaba
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-07 14:28:38 -0200

    Refactors Headline to use functional component

--- app/components/Headline.js
+++ app/components/Headline.js
@@ -1,23 +1,15 @@
-import React, {
-  Component,
-  Text,
-} from 'react-native'
-
+import React, { Text } from 'react-native'
 import Colors from "../styles/Colors"
 
-export default class Headline extends Component {
-  render() {
-    return (
-      <Text style={{
-        color: Colors.brown,
-        fontWeight: 'bold',
-        fontSize: 24,
-        textAlign: 'center',
-        paddingHorizontal: 40,
-        marginBottom: 36,
-      }}>
-        {this.props.children}
-      </Text>
-    )
-  }
-}
+export default Headline = ({children}) => (
+  <Text style={{
+    color: Colors.brown,
+    fontWeight: 'bold',
+    fontSize: 24,
+    textAlign: 'center',
+    paddingHorizontal: 40,
+    marginBottom: 36,
+  }}>
+    {children}
+  </Text>
+)
