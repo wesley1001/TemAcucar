fa6992e7631576d4c7b06dec71002a25f415427d
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-07 14:43:13 -0200

    Refactors Link as a functional component

--- app/components/Link.js
+++ app/components/Link.js
@@ -1,27 +1,16 @@
-import React, {
-  Component,
-  Text,
-  TouchableOpacity,
-} from 'react-native'
-
+import React, { Text, TouchableOpacity } from 'react-native'
 import Colors from "../styles/Colors"
 
-export default class Link extends Component {
-
-  render() {
-    const { style, onPress, children } = this.props
-    return (
-      <TouchableOpacity style={style} onPress={onPress}>
-        <Text style={{
-          paddingHorizontal: 20,
-          paddingVertical: 12,
-          fontSize: 16,
-          color: Colors.brown,
-          textDecorationLine: 'underline',
-        }}>
-          {children}
-        </Text>
-      </TouchableOpacity>
-    )
-  }
-}
+export default Link = ({style, onPress, children}) => (
+  <TouchableOpacity style={style} onPress={onPress}>
+    <Text style={{
+      paddingHorizontal: 20,
+      paddingVertical: 12,
+      fontSize: 16,
+      color: Colors.brown,
+      textDecorationLine: 'underline',
+    }}>
+      {children}
+    </Text>
+  </TouchableOpacity>
+)
