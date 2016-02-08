28daca94f7ec156685bbc2d68fc0a7455ee86c28
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-08 15:13:52 -0200

    Enables passing props to StyledRouter

--- app/components/StyledRouter.js
+++ app/components/StyledRouter.js
@@ -2,13 +2,14 @@ import React from 'react-native'
 import { Router } from 'react-native-router-flux'
 import Colors from "../Colors"
 
-export default StyledRouter = ({ children }) => (
+export default StyledRouter = (props) => (
   <Router 
     navigationBarStyle={{ backgroundColor: Colors.beige }}
     titleStyle={{ color: Colors.brown }}
     barButtonIconStyle={{ tintColor: Colors.brown }}
     barButtonTextStyle={{ color: Colors.brown }}
+    { ...props }
   >
-    {children}
+    { props.children }
   </Router>
 )
