519d9540add2a9dc842c1055f655e452dffb8d87
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-16 18:23:51 -0200

    Removes GiftedForm tests and goes back to where we were

--- index.android.js
+++ index.android.js
@@ -1,18 +1,3 @@
-// import { AppRegistry } from 'react-native'
-// import Root from './app/containers/Root'
-// AppRegistry.registerComponent('TemAcucar', () => Root)
-
-import React, { AppRegistry, Component, View, Text } from 'react-native'
-import { GiftedForm, GiftedFormManager } from 'react-native-gifted-form'
-
-class Root extends Component {
-  render() {
-    return (
-      <View>
-        <Text>ABC</Text>
-      </View>
-    )
-  }
-}
-
+import { AppRegistry } from 'react-native'
+import Root from './app/containers/Root'
 AppRegistry.registerComponent('TemAcucar', () => Root)
