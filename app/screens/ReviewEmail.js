2b7b9f114612f4bf7443e63b500c7561370937d3
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-23 00:36:32 -0300

    Designs ReviewEmail

--- app/screens/ReviewEmail.js
+++ app/screens/ReviewEmail.js
@@ -2,17 +2,16 @@ import React, { Text } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 
 import SimpleScreen from "../components/SimpleScreen"
+import Headline from "../components/Headline"
 import TextBox from "../components/TextBox"
 import OrSeparator from "../components/OrSeparator"
 import Button from "../components/Button"
 
 export default ReviewEmail = ({ currentUser: { email }, onConfirm, onUpdate }) => (
   <SimpleScreen>
-    <TextBox style={{marginBottom: 36}}>
-      <Text style={{fontWeight: 'bold'}}>
-        { `${email} ` }
-      </Text>
-      é o email que você usa no dia-a-dia? Não vamos mandar spam, mas precisamos do seu email para colocar você em contato com seus vizinhos :)
+    <Headline style={{fontSize: 18, marginBottom: 10}}>{email}</Headline>
+    <TextBox style={{marginBottom: 36, textAlign: 'center'}}>
+      Este é o seu email do dia-a-dia? Nunca enviaremos spam, mas precisamos do seu email para te colocar em contato com seus vizinhos :)
     </TextBox>
     <Button onPress={onConfirm}>
       Sim, é este mesmo
