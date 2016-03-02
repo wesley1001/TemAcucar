88ef6f7ecca14a1ce34e70721a406f65e2d878b5
Daniel Weinmann <danielweinmann@gmail.com>
2016-03-02 00:57:00 -0300

    Updates FormScreen to make Form occupy the whole screen

--- app/components/FormScreen.js
+++ app/components/FormScreen.js
@@ -1,18 +1,24 @@
-import React from 'react-native'
+import React, { Platform, View, Image } from 'react-native'
 
 import Colors from "../Colors"
 import SimpleScreen from "./SimpleScreen"
 import Form from "./Form"
 
 export default FormScreen = (props) => (
-  <SimpleScreen viewStyle={{
-    alignItems: 'stretch',
+  <View style={{
     flex: 1,
-    paddingTop: 120,
     backgroundColor: Colors.beige,
+    paddingTop: 40 + (Platform.OS === 'ios' ? 24 : 16),
   }}>
     <Form {...props}>
+      <View style={{
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        paddingTop: 40,
+      }}>
+        <Image source={require('../img/logo.png')} style={{marginBottom: 30}}/>
+      </View>
       { props.children }
     </Form>
-  </SimpleScreen>
+  </View>
 )
