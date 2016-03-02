b4c706ac543107e7923183fbd6fb66fe7b1b7c6c
Daniel Weinmann <danielweinmann@gmail.com>
2016-03-02 02:57:19 -0300

    Configures CodePush for android

--- android/app/src/main/java/com/temacucar/MainActivity.java
+++ android/app/src/main/java/com/temacucar/MainActivity.java
@@ -1,17 +1,30 @@
 package com.temacucar;
 
-import com.lugg.ReactConfig.ReactConfigPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.devfd.RNGeocoder.RNGeocoderPackage;
 import com.facebook.react.ReactActivity;
 import com.AirMaps.AirPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.magus.fblogin.FacebookLoginPackage;
+// 1. Import the plugin class
+import com.microsoft.codepush.react.CodePush;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ReactActivity {
+
+    // 2. Define a private field to hold the CodePush runtime instance
+    private CodePush _codePush;
+
+    // 3. Override the getJSBundleFile method in order to let
+    // the CodePush runtime determine where to get the JS
+    // bundle location from on each app start
+    @Override
+    protected String getJSBundleFile() {
+        return this._codePush.getBundleUrl("index.android.bundle");
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -37,12 +50,18 @@ public class MainActivity extends ReactActivity {
    */
     @Override
     protected List<ReactPackage> getPackages() {
+      // 4. Instantiate an instance of the CodePush runtime, using the right deployment key. If you don't
+      // already have it, you can run "code-push deployment ls <appName> -k" to retrieve your key.
+      this._codePush = new CodePush("8MhfpW1tw8kHs2sH-sONBdmDEY_m4kinL60qx", this, BuildConfig.DEBUG);
+      
+      // 5. Add the CodePush package to the list of existing packages
       return Arrays.<ReactPackage>asList(
-        new ReactConfigPackage(),
+        new ReactNativeConfigPackage(),
         new RNGeocoderPackage(),
         new FacebookLoginPackage(),
         new AirPackage(),
-        new MainReactPackage()
+        new MainReactPackage(),
+        this._codePush.getReactPackage()
       );
     }
 }
