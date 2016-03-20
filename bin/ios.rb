cd3358e07cd21d3475a4844f98eae8662b3e181c
Daniel Weinmann <danielweinmann@gmail.com>
2016-03-20 18:20:43 -0300

    Adds option to build for iphone

---
+++ bin/ios.rb
@@ -0,0 +1,22 @@
+require 'fileutils'
+
+config = {}
+File.open("./.env") do |file|
+  file.each_line do |line|
+    option = line.split("=")
+    config[option[0].to_sym] = option[1].gsub(/\n/, "")
+  end
+end
+
+puts "Generating Info.plist with injected .env vars..."
+file = File.open("./ios/TemAcucar/Info.sample.plist", "rb")
+contents = file.read
+contents.gsub!("`${CODEPUSH_DEPLOYMENT_KEY}`", config[:CODEPUSH_DEPLOYMENT_KEY_IOS])
+contents.gsub!("`${FACEBOOK_APP_ID}`", config[:FACEBOOK_APP_ID])
+File.write("./ios/TemAcucar/Info.plist", contents)
+
+puts "Generating AppDelegate.m with injected .env vars..."
+file = File.open("./ios/TemAcucar/AppDelegate.sample.m", "rb")
+contents = file.read
+contents.gsub!("`${JS_URL}`", config[:JS_URL])
+File.write("./ios/TemAcucar/AppDelegate.m", contents)
---
+++ ios/TemAcucar/AppDelegate.sample.m
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import "CodePush.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#import "AppDelegate.h"
+
+#import "RCTRootView.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+
+  NSURL *jsCodeLocation;
+
+  #ifdef DEBUG
+    jsCodeLocation = [NSURL URLWithString:@"`${JS_URL}`/index.ios.bundle?platform=ios&dev=true"];
+  #else
+    jsCodeLocation = [CodePush bundleURL];
+    // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
+    
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"TemAcucar"
+                                               initialProperties:nil
+                                                   launchOptions:launchOptions];
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  // return YES;
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                    didFinishLaunchingWithOptions:launchOptions];
+}
+
+// Facebook SDK
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    [FBSDKAppEvents activateApp];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                          openURL:url
+                                                sourceApplication:sourceApplication
+                                                       annotation:annotation];
+}
+
+@end
