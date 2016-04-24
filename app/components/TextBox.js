cf943e7d4049d725d5a973702d992844f5d71066
Daniel Weinmann <danielweinmann@gmail.com>
2016-04-24 03:24:40 -0300

    Fixes line height for TextBox on Android

--- app/components/TextBox.js
+++ app/components/TextBox.js
@@ -1,10 +1,10 @@
-import React, { Text } from 'react-native'
+import React, { Platform } from 'react-native'
 import Sentence from "./Sentence"
 
 export default TextBox = (props) => (
   <Sentence style={[{
     marginHorizontal: 30,
-    lineHeight: 16,
+    lineHeight: (Platform.OS === 'ios' ? 16 : 18),
     textAlign: 'center',
   }, props.style]}>
     {props.children}
