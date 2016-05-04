48f73706f9aee170960fb3de08c0e79d4144f9d8
Daniel Weinmann <danielweinmann@gmail.com>
2016-05-04 04:50:17 -0300

    Installs Google Analytics bridge for iOS

--- app/screens/Welcome.js
+++ app/screens/Welcome.js
@@ -1,18 +1,27 @@
-import React from 'react-native'
+import React, { Component } from 'react-native'
+import GoogleAnalytics from 'react-native-google-analytics-bridge'
 import { Actions } from 'react-native-router-flux'
 
 import SimpleScreen from "../components/SimpleScreen"
 import OrSeparator from "../components/OrSeparator"
 import Button from "../components/Button"
 
-export default Welcome = () => (
-  <SimpleScreen headline="Compartilhe coisas com seus vizinhos">
-    <Button onPress={Actions.signUp}>
-      Quero me cadastrar
-    </Button>
-    <OrSeparator />
-    <Button onPress={Actions.signIn}>
-      Já possuo cadastro
-    </Button>
-  </SimpleScreen>
-)
+export default class Welcome extends Component {
+  componentDidMount() {
+    GoogleAnalytics.trackScreenView('Welcome')
+  }
+
+  render() {
+    return(
+      <SimpleScreen headline="Compartilhe coisas com seus vizinhos">
+        <Button onPress={Actions.signUp}>
+          Quero me cadastrar
+        </Button>
+        <OrSeparator />
+        <Button onPress={Actions.signIn}>
+          Já possuo cadastro
+        </Button>
+      </SimpleScreen>
+    )
+  }
+}
--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,7 @@
 import { AppRegistry } from 'react-native'
+import Config from 'react-native-config'
+import GoogleAnalytics from 'react-native-google-analytics-bridge'
 import ProviderContainer from './app/containers/ProviderContainer'
+
+GoogleAnalytics.setTrackerId(Config.GOOGLE_ANALYTICS_ID_IOS)
 AppRegistry.registerComponent('TemAcucar', () => ProviderContainer)
