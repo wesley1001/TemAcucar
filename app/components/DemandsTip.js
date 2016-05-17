4108a62718bf93d2baf43ce3797a361a1f01ff9f
Daniel Weinmann <danielweinmann@gmail.com>
2016-05-17 14:56:41 -0300

    Updates DemandsTip text

--- app/components/DemandsTip.js
+++ app/components/DemandsTip.js
@@ -4,8 +4,8 @@ import ShareTip from "../components/ShareTip"
 
 export default DemandsTip = (props) => {
   const { currentUser, neighborsCount, onShare, onFacebook, facebookConnecting } = props
-  const title = (neighborsCount < 30 && 'Sua vizinhança poderia estar mais cheia. Quanto mais vizinhos, maior a chance de compartilhar objetos e de conhecer pessoas incríveis! Compartilhe no Facebook marcando seus amigos que moram perto de você!')
-  const callToAction = (neighborsCount < 30 && 'Chamar mais vizinhos')
+  const title = (neighborsCount < 30 && 'Quanto mais vizinhos, mais trocas! Compartilhe no Facebook, marcando amigos que moram perto de você.')
+  const callToAction = (neighborsCount < 30 && 'Chame seus vizinhos')
   if ( !currentUser.facebook_uid && neighborsCount >= 30 ) {
     return (<FacebookTip onPress={onFacebook} loading={facebookConnecting} title={title} />)
   }
